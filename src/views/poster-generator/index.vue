98269edafbb779b0b410a524fa17d735a0d9eaaa
lw <80869846+201811510411lw@users.noreply.github.com>
2026-04-27 10:34:41 +0800

    remove center column scrollbar from poster workspace

--- src/views/poster-generator/index.vue
+++ src/views/poster-generator/index.vue
@@ -1,15 +1,13 @@
 <template>
   <AppShell>
-    <div class="grid h-full min-h-0 min-w-0 grid-cols-[320px_minmax(0,1fr)_400px] gap-6 overflow-hidden 2xl:grid-cols-[340px_minmax(0,1fr)_420px]">
+    <div class="grid h-full min-h-0 min-w-0 grid-cols-[320px_minmax(0,1fr)_400px] gap-5 overflow-hidden 2xl:grid-cols-[340px_minmax(0,1fr)_420px]">
       <div class="h-full min-h-0 min-w-0 overflow-hidden">
         <AssetUploadPanel />
       </div>
-      <div class="flex h-full min-h-0 min-w-0 flex-col overflow-hidden">
-        <div class="workspace-scroll min-h-0 flex-1 overflow-y-auto">
-          <div class="flex min-h-full flex-col gap-5 pr-3">
-            <TopConfigBar />
-            <GenerateFormPanel />
-          </div>
+      <div class="flex h-full min-h-0 min-w-0 flex-col overflow-hidden pr-1">
+        <div class="flex h-full min-h-0 flex-col gap-5 overflow-hidden">
+          <TopConfigBar />
+          <GenerateFormPanel />
         </div>
       </div>
       <div class="h-full min-h-0 min-w-0 overflow-hidden">
