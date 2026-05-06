b2f48c593b1e6c05d45ee2f6883a7c4328b269bb
lw <80869846+201811510411lw@users.noreply.github.com>
2026-05-06 10:31:20 +0800

    Enable OpenAI image configuration properties

--- backend-java/src/main/java/com/aiposter/PosterAdminApplication.java
+++ backend-java/src/main/java/com/aiposter/PosterAdminApplication.java
@@ -1,9 +1,12 @@
 package com.aiposter;
 
+import com.aiposter.openai.OpenAiImageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OpenAiImageProperties.class)
 public class PosterAdminApplication {
 
     public static void main(String[] args) {
