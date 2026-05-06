f38ba3111c58b43a665664cebbf2fdab8ce36674
lw <80869846+201811510411lw@users.noreply.github.com>
2026-05-06 10:11:14 +0800

    Support storing generated image bytes

--- backend-java/src/main/java/com/aiposter/storage/StorageService.java
+++ backend-java/src/main/java/com/aiposter/storage/StorageService.java
@@ -5,5 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface StorageService {
     StoredFile store(MultipartFile file, String folder);
 
+    StoredFile store(byte[] content, String originalFilename, String contentType, String folder);
+
     void delete(String storagePath);
 }
